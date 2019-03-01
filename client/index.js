2c2da893025d519873811739b9b67f3e1b36247e
EliudArudo <eliudarudo@gmail.com>
2019-03-01 21:00:34 +0300

    Added error + success message on voting client

--- client/index.js
+++ client/index.js
@@ -1,65 +1,80 @@
-const myId = "sdfljdfsfljnjlsnfljdfns";
+const myId = 'sdfljdfsfljnjlsnfljdfns';
 
 const dog = document.getElementById('dog');
 const cat = document.getElementById('cat');
 
 const input = document.getElementById('vote-input');
 
-function status(element, success) {
+const messageElement = document.getElementById('status-message');
+
+function status(element, success, message) {
     const BACKGROUND = 'rgba(0, 0, 0, 0.1)';
     const SUCCESS_BACKGROUND = 'rgba(117, 235, 0, 0.1)';
     const FAIL_BACKGROUND = 'rgba( 255, 64, 46, 0.5)';
 
-    let background;
+    let background,
+        Class;
     if (success) {
+        Class = 'success';
         background = SUCCESS_BACKGROUND;
     } else {
+        Class = 'error';
         background = FAIL_BACKGROUND;
     }
+    messageElement.classList.add(Class);
 
     element.style.background = background;
     input.value = null;
+    messageElement.innerText = message;
 
     setTimeout(() => {
+        messageElement.classList.remove(Class);
         element.style.background = BACKGROUND;
+        messageElement.innerText = null;
     }, 1000);
-
 }
 
 function vote(vote, id) {
-
     /// Voter-Client - '/' :3000
     /// Results  - '/results' :4000
     /// Worker - :5000 '/api' - should be perfectly okay, for use with other interfaces
     axios({
-        url: '/api/vote',
-        method: 'post',
-        data: {
-            id,
-            vote
-        }
-    }).then(res => {
-        console.log(res.data);
-        status(input, true);
-    }).catch(e => {
-        console.log(e.response);
-        status(input, false);
-    })
+            url: '/api/vote',
+            method: 'post',
+            data: {
+                id,
+                vote
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            if (res.data.length === 0) {
+                return status(input, true, 'Sorry, please try again later');
+            }
+            status(input, true, res.data);
+        })
+        .catch((e) => {
+            console.log(e.response);
+            if (e.response.data.length === 0) {
+                return status(input, false, "Sorry, we're experiencing some issues");
+            }
+
+            status(input, false, e.response.data);
+        });
 }
 
-dog.addEventListener("click", () => {
-
-    if (!input || input.value.trim() === "") {
-        status(input, false);
+dog.addEventListener('click', () => {
+    if (!input || input.value.trim() === '') {
+        status(input, false, 'Please enter your id');
         return;
     }
 
     vote('dog', input.value);
 });
 
-cat.addEventListener("click", () => {
-    if (!input || input.value.trim() === "") {
-        status(input, false);
+cat.addEventListener('click', () => {
+    if (!input || input.value.trim() === '') {
+        status(input, false, 'Please enter your id');
         return;
     }
     vote('cat', input.value);
